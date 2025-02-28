99a74fc22f16db722b20068fdcdf81612074141a
theN1R0 <nickiita49@gmail.com>
2025-03-01 00:44:03 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-fogit init
+  nt-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
